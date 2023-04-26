2e0d2440c8169527d3619f0bb533cba58608eeeb
Saifahmeed <122873799+Saifahmeed@users.noreply.github.com>
2023-04-26 21:35:04 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,5 +1,5 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef NEW_SHELL_H
+#define NEW_SHELL_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,67 +12,59 @@
 #include <dirent.h>
 #include <signal.h>
 
+/constants/
+#define CMD_TYPE_EXTERNAL 1
+#define CMD_TYPE_INTERNAL 2
+#define CMD_TYPE_PATH 3
+#define CMD_TYPE_INVALID -1
 
-#define EXTERNAL_COMMAND 1
-#define INTERNAL_COMMAND 2
-#define PATH_COMMAND 3
-#define INVALID_COMMAND -1
-
-#define min(x, y) (((x) < (y)) ? (x) : (y))
-
-/**
- *struct map - a struct that maps the
- *command name into its right function 
- *@command_name: command name
- *@func: the function that executes the command
- */
-
-typedef struct map
+#define new_min(x, y) (((x) < (y)) ? (x) : (y))
+typedef struct cmd_map
 {
-char *command_name;
+char *cmd_name;
 void (*func)(char **command);
-} map;
+} cmd_map_t;
 
-extern char **environ;
-extern char *line;
-extern char **commands;
+extern char **environment;
+extern char *input_line;
+extern char **input_commands;
 extern char *shell_name;
 extern int status;
 
-/*helpers1.c*/
-void print(char *, int);
-char **tokin_maker(char *, char *);
-void remove_newline(char *);
-int _strlen(char *);
-void _strcpy(char *, char *);
+/helpers/
+void print_message(char *, int);
+char **tokenize_string(char *, char *);
+void remove_newline_character(char *);
+int get_string_length(char *);
+void copy_string(char *, char *);
 
-/*helpers2.c*/
-int _strcomp(char *, char *);
-char *_strconnect(char *, char *);
-int _strspn(char *, char *);
-int _strcspn(char *, char *);
-char *_strsearch(char *, char);
+/helpers2/
+int compare_strings(char *, char *);
+char *concatenate_strings(char *, char *);
+int get_prefix_length(char *, char *);
+int get_not_matching_prefix_length(char *, char *);
+char *find_char(char *, char);
 
-/*helpers3.c*/
-char *_strtok_r(char *, char *, char **);
-int _atoi(char *);
-void *_realloc(void *p, unsigned int old, unsigned int new_s);
-void ctrl_c_handler(int);
-void remove_comment(char *);
+/helpers3/
+char *tokenize_string_r(char *, char *, char **);
+int convert_string_to_integer(char *);
+void *reallocate_memory(void *ptr, unsigned int old_size, unsigned int new_size);
+void ctrl_c_signal_handler(int);
+void remove_comments(char *);
 
-/*utils.c*/
-int parse_cmd(char *);
-void execute_cmd(char **, int);
-char *check_path(char *);
-void (*get_func(char *))(char **);
-char *_getenv(char *);
+/utils/
+int parse_input_command(char *);
+void execute_input_command(char **, int);
+char *find_command_in_path(char *);
+void (*get_command_function(char *))(char **);
+char *get_environment_variable(char *);
 
-/*built_in_functions.c*/
-void env(char **);
-void quit(char **);
+/built_in/
+void show_environment_variables(char **);
+void exit_shell(char **);
 
-/*main functions*/
-extern void non_interactive(void);
-extern void initializer(char **current_command, int type_command);
+/main/
+extern void run_shell_interactively(void);
+extern void initialize_shell(char **current_command, int cmd_type);
 
-#endif
+#endif /NEW_SHELL_H/
