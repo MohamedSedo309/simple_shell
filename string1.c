7c0efd7342b90021b8e06c17ea8be473e1373e43
MohamedSedo309 <mohamedsedo1907@gmai.com>
2023-04-28 11:46:20 -0700

    string handler 2

---
+++ string1.c
@@ -0,0 +1,82 @@
+#include "shell.h"
+
+/**
+ *copy_string - copy a string
+ *@dest: the destination
+ *@src: the source
+ *Return: pointer to destination
+ */
+char *copy_string(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ *duplicate_string - duplicate string
+ *@str: the string
+ *Return: duplicated string pointer
+ */
+char *duplicate_string(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
+
+/**
+ *_putchar - print char
+ *@c: The char
+ *Return: On success 1.
+ */
+int _putchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+ *print_string - print string
+ *@str: string to print
+ *Return: void
+ */
+void print_string(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
